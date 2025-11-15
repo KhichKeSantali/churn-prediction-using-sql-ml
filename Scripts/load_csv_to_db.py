8038092974270d46d356648f42e5c14c2cd815f8
KhichKeSantali <unstoppablejboss1@gmail.com>
2025-11-14 20:04:51 -0700

    Fixed functionality using config file

--- Scripts/load_csv_to_db.py
+++ Scripts/load_csv_to_db.py
@@ -1,12 +1,16 @@
+import os
 import pandas as pd
 from sqlalchemy import create_engine
+from config import DATABASE_PATH
 
 # Load CSV data into a SQLite database
-df = pd.read_csv('data/Telco-Customer-Churn.csv')
+data_path = os.path.join(os.getcwd(), '..', 'data', 'Telco-Customer-Churn.csv')
+df = pd.read_csv(data_path)
 
 # Create a SQLite database connection
 try:
-    engine = create_engine('sqlite:///database/churn.db')
+    db_path = os.path.join(os.getcwd(), 'database', 'churn.db')
+    engine = create_engine(f"sqlite:///{DATABASE_PATH}")
     print(f"Data loaded into the database successfully. {len(df)} records inserted.")
 except Exception as e:
     print(f"Error creating database connection: {e}")
@@ -16,4 +20,3 @@ except Exception as e:
 # Load data into the 'customers' table
 df.to_sql('customers', engine, index=False, if_exists='replace')
 
-# Confirm successful load
